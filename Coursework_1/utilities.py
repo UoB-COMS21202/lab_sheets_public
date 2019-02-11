f8ff59448408eb918ae5df6b811be2d802bcdacb
Michael Wray <Michael.Wray@bristol.ac.uk>
2019-02-11 11:52:21 +0000

    Adds Coursework 1

---
+++ Coursework_1/utilities.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+def load_points_from_file(filename):
+    """Loads 2d points from a csv called filename
+    Args:
+        filename : Path to .csv file
+    Returns:
+        (xs, ys) where xs and ys are a numpy array of the co-ordinates.
+    """
+    points = pd.read_csv(filename, header=None)
+    return points[0].values, points[1].values
+
+
+def view_data_segments(xs, ys):
+    """Visualises the input file with each segment plotted in a different colour.
+    Args:
+        xs : List/array-like of x co-ordinates.
+        ys : List/array-like of y co-ordinates.
+    Returns:
+        None
+    """
+    assert len(xs) == len(ys)
+    assert len(xs) % 20 == 0
+    len_data = len(xs)
+    num_segments = len_data // 20
+    colour = np.concatenate([[i] * 20 for i in range(num_segments)])
+    plt.set_cmap('Dark2')
+    plt.scatter(xs, ys, c=colour)
+    plt.show()
